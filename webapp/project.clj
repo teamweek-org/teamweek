c8ab4a2d7768b62fed448204082abb3fbe40a414
Iván Perdomo <ivan@perdomo.me>
2015-12-05 09:34:03 +0100

    Webapp skeleton

---
+++ webapp/project.clj
@@ -0,0 +1,6 @@
+(defproject org.teamweek.webapp "0.1.0-SNAPSHOT"
+  :description "Teamweek Webapp"
+  :url "https://github.com/teamweek-org/teamweek"
+  :license {:name "Mozilla Public License 2.0"
+            :url "https://www.mozilla.org/en-US/MPL/2.0/"}
+  :dependencies [[org.clojure/clojure "1.7.0"]])
