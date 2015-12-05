aea45ed7cf716037b38804bdce4d55cd63cde822
Jonas Enlund <jonas.enlund@gmail.com>
2015-12-05 14:51:31 +0200

    Add shutdown hooks + some additional deps

--- slackbot/project.clj
+++ slackbot/project.clj
@@ -3,10 +3,13 @@
   :url "https://teamweek.org"
   :license {:name "Mozilla Public License, version 2.0"
             :url "https://www.mozilla.org/en-US/MPL/2.0/"}
+  :pedantic? true
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.async "0.2.374"]
                  [org.clojure/tools.nrepl "0.2.12"]
+                 [com.datomic/datomic-free "0.9.5344"]
                  [aleph "0.4.1-beta3"]
-                 [cheshire "5.5.0"]]
+                 [cheshire "5.5.0"]
+                 [clojurewerkz/quartzite "2.0.0"]]
   :main ^:skip-aot org.teamweek.slackbot
   :profiles {:uberjar {:aot :all}})
--- slackbot/src/org/teamweek/slackbot.clj
+++ slackbot/src/org/teamweek/slackbot.clj
@@ -13,8 +13,8 @@
 (defn connect-url [token]
   (str "https://slack.com/api/rtm.start?token=" token))
 
-(defn keep-alive [ws-conn]
-  (future (while true
+(defn keep-alive [ws-conn shutdown?]
+  (future (while (not @shutdown?)
             (Thread/sleep 5000)
             @(stream/put! ws-conn
                           (json/generate-string {:id (rand-int 1e6)
@@ -30,8 +30,8 @@
 (defn direct-message? [msg]
   (.startsWith (:channel msg) "D"))
 
-(defn route-messages [conn incoming-messages]
-  (future (while true
+(defn route-messages [conn incoming-messages shutdown?]
+  (future (while (not @shutdown?)
             (let [msg (json/parse-string @(stream/take! (:ws-conn conn)) true)]
               (condp = (:type msg)
                 "message" (when (and (not= (:user msg) (:id (:self conn)))
@@ -41,12 +41,12 @@
                                                :username (find-username conn (:user msg)))))
                 (do #_ignore))))))
 
-(defn websocket-client [url incoming-messages]
+(defn websocket-client [url shutdown?]
   (let [conn @(http/websocket-client url)
         first-msg (json/parse-string @(stream/take! conn))]
     (when-not (= first-msg {"type" "hello"})
       (throw (ex-info "Unable to connect" first-msg)))
-    (keep-alive conn)
+    (keep-alive conn shutdown?)
     conn))
 
 
@@ -58,18 +58,25 @@
                       :body
                       byte-streams/to-string
                       (json/parse-string true))
+        shutdown? (atom false)
         incoming-messages (async/chan)
         dm-pub (async/pub incoming-messages
                           (fn topic-fn [msg]
                             (:username msg)))
         ws-conn (websocket-client (:url conn-data)
-                                  incoming-messages)
-
+                                  shutdown?)
+        shutdown-fn (fn []
+                      (println "Shutting down" (:url conn-data))
+                      (reset! shutdown? true)
+                      (async/close! incoming-messages)
+                      (stream/close! ws-conn)
+                      (println "ok."))
         conn (assoc conn-data
                     :ws-conn ws-conn
                     :incoming-messages incoming-messages
-                    :dm-pub dm-pub)]
-    (route-messages conn incoming-messages)
+                    :dm-pub dm-pub
+                    :shutdown-fn shutdown-fn)]
+    (route-messages conn incoming-messages shutdown?)
     conn))
 
 (defn find-user-dm-id [conn user]
@@ -128,13 +135,14 @@
 (comment
   (def conn (connect slack-token))
 
-  conn
+  ((:shutdown-fn conn))
 
-  (send-to-user! conn "jonas" "Send me a message!")
+  (send-to-user! conn "jonas" "ping!")
 
 
   (async/thread
-    (let [answers (send-questionnaire conn "jonas" ["How are you today?" "What are your plans for tomorrow?"])]
+    (let [answers (send-questionnaire conn "jonas" ["How are you today?"
+                                                    "What are your plans for tomorrow?"])]
       (send-to-user! conn "jonas" (str "You answered: " (pr-str answers)))))
 
   )
