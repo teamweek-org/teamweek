54ba57103a76b8f51bdde3700683529cafcc174a
Jonas Enlund <jonas.enlund@gmail.com>
2015-12-05 07:34:50 +0200

    Connection established

--- slackbot/project.clj
+++ slackbot/project.clj
@@ -3,6 +3,9 @@
   :url "https://teamweek.org"
   :license {:name "Mozilla Public License, version 2.0"
             :url "https://www.mozilla.org/en-US/MPL/2.0/"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/tools.nrepl "0.2.12"]
+                 [aleph "0.4.1-beta3"]
+                 [cheshire "5.5.0"]]
   :main ^:skip-aot org.teamweek.slackbot
   :profiles {:uberjar {:aot :all}})
--- slackbot/src/org/teamweek/slackbot.clj
+++ slackbot/src/org/teamweek/slackbot.clj
@@ -1,5 +1,30 @@
 (ns org.teamweek.slackbot
+  (:require [aleph.http :as http]
+            [manifold.stream :as stream]
+            [cheshire.core :as json]
+            [byte-streams])
   (:gen-class))
+
+;; for testing
+(defonce slack-token (or (System/getenv "SLACK_TOKEN")
+                         (throw (ex-info "No SLACK_TOKEN provided" {}))) )
+
+(defn connect-url
+  "Given a slack token, returns a websocket url"
+  [token]
+  (-> (str "https://slack.com/api/rtm.start?token=" token)
+      (http/get)
+      deref
+      :body
+      byte-streams/to-string
+      json/parse-string
+      (get "url")))
+
+(comment
+  (let [conn @(http/websocket-client (connect-url slack-token))]
+    @(stream/take! conn))
+
+  )
 
 (defn -main [& args]
   (println "Hello world!"))
