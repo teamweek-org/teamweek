6c8e14f2a57c404ef4c0e380224e966c4a085813
Iván Perdomo <ivan@perdomo.me>
2015-12-06 12:36:59 +0100

    Progress in search

--- webapp/src/org/teamweek/webapp/endpoint/search.clj
+++ webapp/src/org/teamweek/webapp/endpoint/search.clj
@@ -1,8 +1,20 @@
 (ns org.teamweek.webapp.endpoint.search
-  (:require [compojure.core :refer :all]))
+  (:require [compojure.core :refer :all]
+            [ring.util.response :refer :all]
+            [datomic.api :as d]))
 
 
 
 (defn search-endpoint [config]
   (context "/search" []
-   (GET "/" [] "Hello World")))
+   (GET "/" req
+     (let [t (get-in req [:params :t])
+           q (get-in req [:params :q])
+           db (:db req)]
+       (if t
+         (if-let [eid (ffirst (d/q '[:find ?e :in $ ?t :where [?e :team/token ?t]] db t))]
+           (let [team (d/entity db eid)
+                 domain (:team/domain team)]
+             domain)
+           (redirect "/"))
+         (redirect "/"))))))
