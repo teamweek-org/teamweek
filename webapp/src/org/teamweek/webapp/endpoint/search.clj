4a0a0645e132b7561f0504a3fb1f946fba9af1d7
Iván Perdomo <ivan@perdomo.me>
2015-12-06 14:17:46 +0100

    Search skeleton is ready!

--- webapp/src/org/teamweek/webapp/endpoint/search.clj
+++ webapp/src/org/teamweek/webapp/endpoint/search.clj
@@ -4,9 +4,19 @@
             [datomic.api :as d]
             [clj-http.client :as http]
             [cheshire.core :as json]
-            [hiccup.core :refer :all]))
+            [hiccup.core :refer :all]
+            [clojure.string :as str]))
 
 
+(defn get-results
+  [domain q]
+  (if (str/blank? q)
+    []
+    (let [query {:body (json/encode {:query {:query_string {:analyze_wildcard "true"
+                                                                  :query q}}})}]
+      (-> (http/get (str "http://localhost:9200/" domain "/_search") query) ;;TODO remove hardcoded ES_URI
+          :body
+          (json/parse-string true)))))
 
 (defn search-endpoint [config]
   (context "/search" []
@@ -18,13 +28,12 @@
          (if-let [eid (ffirst (d/q '[:find ?e :in $ ?t :where [?e :team/token ?t]] db t))]
            (let [team (d/entity db eid)
                  domain (:team/domain team)
-                 query {:body (json/encode {:query {:query_string {:analyze_wildcard "true"
-                                                                   :query q}}})}
-                 result (-> (http/get (str "http://localhost:9200/" domain "/_search") query) ;;TODO remove hardcoded ES_URI
-                            :body
-                            (json/parse-string true))]
-
+                 result (get-results domain q)]
              (html
+               [:form {:action "/search" :method "get"}
+                [:input {:type "hidden" :name "t" :value t}]
+                [:input {:name "q" :value q}]
+                [:input {:type "submit" :name "submit"}]]
                [:ul
                 (for [hit (get-in result [:hits :hits])]
                   [:li (:text (:_source hit))])]))
